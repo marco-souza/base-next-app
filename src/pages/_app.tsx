754a8ae92d78d4c0c5f6a488580db0dabeabfb5a
Marco Antonio <masjr@amazon.com>
2020-07-19 12:43:07 -0300

    FEA: add Add RetroWaveSction component
    - add styled-components
    - add helmet
    - add font styles
    - add antd navbar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,36 +1,29 @@
-import './styles.css';
-
+import { Helmet } from 'react-helmet';
 import { Layout, Breadcrumb, Menu } from 'antd';
 import SideMenu from 'components/SideMenu';
+import 'pages/styles.css';
 
-const { Header, Content, Footer, Sider } = Layout;
+const { Header, Content, Footer } = Layout;
 
 export default function PageLayout({ Component, pageProps }) {
   return (
-    <Layout style={{ minHeight: '100vh' }}>
-      <Header className="header">
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item key="1">nav 1</Menu.Item>
-          <Menu.Item key="2">nav 2</Menu.Item>
-          <Menu.Item key="3">nav 3</Menu.Item>
-        </Menu>
-      </Header>
-      <Layout>
-        <SideMenu />
-        <Layout className="site-layout">
-          <Content style={{ margin: '0 16px' }}>
-            <Breadcrumb style={{ margin: '16px 0' }}>
-              <Breadcrumb.Item>User</Breadcrumb.Item>
-              <Breadcrumb.Item>Bill</Breadcrumb.Item>
-            </Breadcrumb>
-            <div className="site-layout-background" style={{ padding: 24, minHeight: 360 }}>
-              <Component {...pageProps} />
-            </div>
-          </Content>
-          <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
-        </Layout>
+    <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>PodCodar Code School</title>
+      </Helmet>
+
+      <Layout style={{ minHeight: '100vh' }}>
+        <Header className="header">
+          <div className="logo" />
+          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+            <Menu.Item key="1">nav 1</Menu.Item>
+            <Menu.Item key="2">nav 2</Menu.Item>
+            <Menu.Item key="3">nav 3</Menu.Item>
+          </Menu>
+        </Header>
+        <Component {...pageProps} />
       </Layout>
-    </Layout>
+    </>
   );
 }
